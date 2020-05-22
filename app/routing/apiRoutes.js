84c972b069ec4856eb2c01af8efbe4ae478d7fe5
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-05-22 17:02:31 -0600

    added to the api routes a bit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,19 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
+
+        console.log("posting...")
         var newFriend = req.body;
+
+        var newScore = function(array){
+            var newScore = [];
+            for (var i = 0; i < array.length; i++){
+                newScore.push(parseInt(array[i]));
+            }
+            return newScore
+        }
+
+
 
         // Using a RegEx Pattern to remove spaces from newCharacter
         // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
