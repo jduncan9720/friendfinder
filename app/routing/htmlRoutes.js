50430f7f7d23620910c25fbd38302a72591b0267
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-05-27 16:05:26 -0600

    Added a css file and moved things around to route it correctly

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,11 @@ var path = require("path");
 module.exports = function (app) {
     // Create a route for displaying the home page
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/home.html"));
     });
 
     // Create a route to go to the survey page
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/survey.html"));
     });
 }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 // var routes = require("./htmlRoutes.js");
 
 var app = express();
@@ -7,9 +8,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, './app/public')));
 
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
 
 
 app.listen(PORT, function() {
